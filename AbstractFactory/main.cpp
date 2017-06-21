4f4c60f199d343ab834a323ff225e0d1abe024b5
Henry Warhurst <hwar3828@uni.sydney.edu.au>
2017-06-21 18:11:12 +1000

    First commit

---
+++ AbstractFactory/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  21/06/2017 18:08:48
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+int main() 
+{
+    std::cout << "HI!" << std::endl;
+    return 0;
+}
